4e960421daaa536284fe3fead229d6a358cb7256
Android228666 <asus261235@gmail.com>
2021-12-12 22:51:03 +0200

    some updates

--- code.js
+++ code.js
@@ -2,8 +2,64 @@ function qadrator(a,b,c){
     let D = b*b-4*a*c;
     let x1 = (-b + Math.sqrt(D))/2;
     let x2 = (-b - Math.sqrt(D))/2;
-    console.log(`${a}x^2+${b}x${c}`);
-    console.log(D,x1,x2)
-}
+    // if(a !== "1" && b !== "1" && a !== "0" && b !== "0" && a !== "-1" && b !== "-1"){
+    //     console.log(`${a}x^2${b}x${c}`);
+    // } 
+    // else if(a === "1" && b !== "1" && b != "-1"){
+    //     console.log(`x^2${b}x${c}`);
+    // }
+    //   else if(b === "+1" && a !== "1" && a !== "-1"){
+    //     console.log(`${a}x^2+x${c}`);
+    // } 
+    //   else if(a === "1" && b === "1"){
+    //     console.log(`x^2+x${c}`);
+    // }
+    //   else if(a === "-1" && b === "-1"){
+    //     console.log(`-x^2-x${c}`);
+    // }
+    //   else if(a === "-1" && b === "+1"){
+    //     console.log(`-x^2+x${c}`);
+    // }
+    //   else if(a === "1" && b === "-1"){
+    //     console.log(`x^2-x${c}`);
+    // }
+    //   else if(a === "-1"){
+    //     console.log(`-x^2${b}x${c}`);
+    // } 
+    //   else if(b === "-1"){
+    //     console.log(`${a}x^2-x${c}`);
+    // } 
+    if(a > 0 && b > 0 && c > 0 && a !== 1 && b !== 1 && c !== 1){
+      console.log()
+    } else if(a < 0 && b > 0 && c > 0 && a !== 1 && b !== 1 && c !== 1){
+      console.log(`${a}(x^2)+${b}x+${c}`)
+    } else if(a < 0 && b < 0 && c > 0 && a !== 1 && b !== 1 && c !== 1){
+      console.log(`${a}(x^2)${b}x+${c}`)
+    } else if(a < 0 && b < 0 && c < 0 && a !== 1 && b !== 1 && c !== 1){
+      console.log(`${a}(x^2)${b}x${c}`)
+    } else if(a > 0 && b < 0 && c < 0 && a !== 1 && b !== 1 && c !== 1){
+      console.log(`${a}(x^2)${b}x${c}`)
+    } else if(a > 0 && b < 0 && c > 0 && a !== 1 && b !== 1 && c !== 1){
+      console.log(`${a}(x^2)${b}x+${c}`)
+    } else if(a > 0 && b > 0 && c < 0 && a !== 1 && b !== 1 && c !== 1){
+      console.log(`${a}(x^2)+${b}x${c}`)
+    } else if(a < 0 && b > 0 && c < 0 && a !== 1 && b !== 1 && c !== 1){
+      console.log(`${a}(x^2)+${b}x${c}`)
+    }
+    if(D > 0){
+        console.log(`D = ${D}`);
+        console.log(`x1 = ${x1}`);
+        console.log(`x2 = ${x2}`);
+    } else if( D === 0){
+        console.log(x1 = -b / 2 * a);
 
-console.log(qadrator(1,5,-6))
+    } else{
+        console.log("The equation has no roots");
+    }
+
+
+    
+    
+
+  }
+console.log(qadrator(-3,5,-6))
